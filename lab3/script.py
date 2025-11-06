f9e84101835c55f08ba5a8bbd58222f0e7af9719
john Doe <johndoe@example.com>
2025-11-06 11:16:43 +0000

    feature_part2 branch bad commit

---
+++ lab3/script.py
@@ -0,0 +1,12 @@
+print("This is the main branch - part 1")
+print("Feature branch: part 1 of additional code")
+x = 5
+y = 10
+print("Sum:", x + y)
+print("Feature branch: part 2 of additional code")
+for i in range(3):
+    print("Iteration:", i)
+# Intentionally broken code
+print("Bad commit example"
+for i in range(5)
+print(i)
